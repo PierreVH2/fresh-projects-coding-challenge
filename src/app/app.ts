bfff75ef28018003456f9e052f5813ca6c463244
Pierre van Heerden <pierre.van.heerden86@gmail.com>
2026-03-17 22:43:36 +0200

    Wire up floor plan component demo

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,16 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { FloorPlan } from './floor-plan/floor-plan';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, FloorPlan],
+  imports: [FloorPlan],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('fresh-projects');
+
+  handleRoomClick(roomName: string) {
+    console.log('Clicked on room:', roomName);
+  }
 }
